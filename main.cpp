382a869499a3144185764c6560eed5cb89f1bb8b
Tom Schoenfelder <mod4@gmx.de>
2024-06-27 23:44:36 +0200

    Simple one dimentional movement

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 enum GameScreen {PAUSE, GAMEPLAY};
 
+struct Actor {
+    int posX;
+    int posY;
+
+    float dirX;
+    float dirY;
+
+    float vel;
+
+    int height;
+    int width;
+};
+
 int main()
 {
     // Initialization
@@ -14,6 +27,8 @@ int main()
     GameScreen currentScreen = PAUSE;
 
     rl::SetTargetFPS(60);
+
+    Actor player{.posX=screenWidth/2, .posY=screenHeight/2, .dirX=1, .dirY=0, .vel=0, .height=30, .width=50};
 
     // Main game loop
     while (!rl::WindowShouldClose())    // Detect window close button or ESC key
@@ -34,7 +49,37 @@ int main()
                 if (rl::IsKeyPressed(rl::KEY_P))
                 {
                     currentScreen = PAUSE;
+                    break;
                 }
+
+                // Steering
+                if (rl::IsKeyDown(rl::KEY_LEFT)) {
+
+                }
+
+
+                // Acceleration/Deceleration
+                if (rl::IsKeyDown(rl::KEY_UP)) {
+                    player.vel += .5f;
+                    if (player.vel > 5) player.vel = 5;
+                }
+                else if (rl::IsKeyDown(rl::KEY_DOWN)) {
+                    player.vel -= .5f;
+                    if (player.vel < -2) player.vel = -2;
+                }
+                else {
+                    player.vel -= .1f;
+                    if (player.vel < 0) player.vel = 0;
+                }
+                player.posX += player.vel * player.dirX;
+                player.posY += player.vel * player.dirY;
+
+                // Endless screen
+                if (player.posX > screenWidth)  player.posX = 0;
+                if (player.posX < 0)            player.posX = screenWidth;
+                if (player.posY > screenHeight) player.posY = 0;
+                if (player.posY < 0)            player.posY = screenHeight;
+
             } break;
             default: break;
         }
@@ -55,9 +100,8 @@ int main()
                 } break;
                 case GAMEPLAY:
                 {
-                    rl::DrawRectangle(0, 0, screenWidth, screenHeight, rl::PURPLE);
+                    rl::DrawRectangle(player.posX, player.posY, player.width, player.height, rl::PURPLE);
                     rl::DrawText("GAMEPLAY SCREEN", 20, 20, 40, rl::MAROON);
-                    rl::DrawText("PRESS ENTER to JUMP to ENDING SCREEN", 130, 220, 20, rl::MAROON);
 
                 } break;
                 default: break;
